99a908e9f12ec7501ece1ec6dffe0ef11f43728b
brewz kee <brewzkee1@gmail.com>
2023-08-24 15:30:58 -0500

    fix search field

--- scripts/index.js
+++ scripts/index.js
@@ -21,50 +21,61 @@ function addFruitsToMenu(targetEl) {
 const customListEl = document.getElementsByClassName('customList')[0];
 const component = customList.bind(customListEl)
 
-function customList(arr) {
+function customList(originalArray) {
     const inputEl = this.getElementsByTagName('INPUT')[0];
     const ulEl = this.getElementsByTagName('UL')[0];
-    let arrCopy = arr.map((item, i) => {
-        return { id: i, data: item, selected: false };
-    });
-    let itemsArr = [...arrCopy];
 
-    itemsArr.forEach((item) => {
+    // create a new arr of objects with IDs (assuming the original is not an arr of objects)
+    let menuItemsArr = originalArray.map((item, i) => {
+        return { id: i, data: item };
+    });
+
+    // use menuItemsArr to create LI and A tags and add to UL
+    menuItemsArr.forEach(item => {
         const liEl = document.createElement('li');
         const aEl = document.createElement('a');
 
         aEl.setAttribute('href', '#');
+        aEl.setAttribute('data-menu-selected', 'false');
         aEl.innerText = item.data;
-        aEl.addEventListener('click', function (e) {
-            const obj = arrCopy.find(o => o.id === item.id);
-            obj.selected = !obj.selected;
+        aEl.addEventListener('click', function () {
+            console.log('toggle data attr here')
         });
 
         liEl.appendChild(aEl);
         ulEl.appendChild(liEl);
     });
 
-    inputEl.addEventListener('keyup', function (e) {
-        let tmp = arrCopy.filter(item => {
-            let i = item.data.toLowerCase();
-            let v = this.value.toLowerCase();
-            if (i.includes(v)) return item;
-        });
-
-        itemsArr = tmp;
-        if (itemsArr.length) createLIs(ulEl, itemsArr);
+    //update menu items based on search string
+    inputEl.addEventListener('keyup', function () {
+        updateMenuSelection(this.value);
     });
 
-    function createLIs(containerEl, arr) {
-        const tmp = [...arr];
+    function updateMenuSelection(inputTxtValue) {
+        const LIs = Array.from(ulEl.children);
 
-        containerEl.textContent = '';
+        // if the text field is filled out display menu items that match it,
+        // else display all menu items
+        if (inputTxtValue !== '') {
+            LIs.forEach(LI => {
+                const a = LI.children[0];
 
-        tmp.forEach(item => {
-            const liEl = document.createElement('li');
-            liEl.innerText = item.data;
-            containerEl.appendChild(liEl);
-        });
+                if ((a.innerText).toLowerCase().includes((inputTxtValue).toLowerCase())) {
+                    a.dataset.menuSelected = 'true';
+                    LI.style.display = 'block';
+                } else {
+                    a.dataset.menuSelected = 'false';
+                    LI.style.display = 'none';
+                }
+            })
+        } else {
+            LIs.forEach(LI => {
+                const a = LI.children[0];
+
+                a.dataset.menuSelected = 'false';
+                LI.style.display = 'block';
+            });
+        }
     }
 }
 
@@ -75,4 +86,3 @@ component(fruitsArr);
 // TODO LIs should be tabbable
 // TODO add button to toggle list items (for now click it is ok)
 // TODO remember to remove event listeners
-// BUG updated list doesn't have links
