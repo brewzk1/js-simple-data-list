87134c189d195ff9384905a8fd16f6e6c09f6505
brewz kee <brewzkee1@gmail.com>
2023-08-26 19:53:09 -0500

    add warning if no data

--- scripts/index.js
+++ scripts/index.js
@@ -20,88 +20,92 @@ function addFruitsToMenu(targetEl) {
 // custom
 const clickedOutside = new Event('clickedOutside');
 const customListEl = document.getElementsByClassName('customList')[0];
-const component = customList.bind(customListEl)
+const component = customList.bind(customListEl);
 
 function customList(originalArray) {
-    const INPUT_TEXT = this.getElementsByTagName('INPUT')[0];
-    const DIV = this.getElementsByTagName('DIV')[0];
+    if (originalArray.length > 0) {
+        const INPUT_TEXT = this.getElementsByTagName('INPUT')[0];
+        const DIV = this.getElementsByTagName('DIV')[0];
 
-    // listen for custom event and hide menu if its showing
-    this.addEventListener('clickedOutside', function () {
-        if (DIV.style.display = 'block') DIV.style.display = 'none';
-    });
-
-    // create a new arr of objects with IDs (assuming the original is not an arr of objects)
-    const menuItemsArr = originalArray.map((item, i) => {
-        return { id: i, data: item };
-    });
-
-    // use menuItemsArr to create A tags and add to DIV
-    menuItemsArr.forEach(item => {
-        const A = document.createElement('A');
-
-        A.textContent = item.data;
-        A.setAttribute('href', '#');
-        A.setAttribute('data-menu-selected', 'false');
-        A.addEventListener('click', function () {
-            let menuState = this.dataset.menuSelected === 'false' ? 'true' : 'false';
-
-            this.setAttribute('data-menu-selected', menuState);
-            updateInputField();
+        // listen for custom event and hide menu if its showing
+        this.addEventListener('clickedOutside', function () {
+            if (DIV.style.display = 'block') DIV.style.display = 'none';
         });
 
-        DIV.appendChild(A);
-    });
+        // create a new arr of objects with IDs (assuming the original is not an arr of objects)
+        const menuItemsArr = originalArray.map((item, i) => {
+            return { id: i, data: item };
+        });
 
-    // updatee items in the input field when menu items are clicked
-    function updateInputField() {
-        const As = Array.from(DIV.children);
-        let selectedItems = [];
+        // use menuItemsArr to create A tags and add to DIV
+        menuItemsArr.forEach(item => {
+            const A = document.createElement('A');
 
-        selectedItems = As.filter(A => A.dataset.menuSelected === 'true');
+            A.textContent = item.data;
+            A.setAttribute('href', '#');
+            A.setAttribute('data-menu-selected', 'false');
+            A.addEventListener('click', function () {
+                let menuState = this.dataset.menuSelected === 'false' ? 'true' : 'false';
 
-        if (selectedItems.length > 0) {
-            let items = '';
-
-            selectedItems.forEach((o, i) => {
-                if (i === selectedItems.length - 1) {
-                    items += o.innerText;
-                } else {
-                    items += o.innerText + ', ';
-                }
+                this.setAttribute('data-menu-selected', menuState);
+                updateInputField();
             });
 
-            INPUT_TEXT.value = items;
-        } else {
-            INPUT_TEXT.value = '';
+            DIV.appendChild(A);
+        });
+
+        // updatee items in the input field when menu items are clicked
+        function updateInputField() {
+            const As = Array.from(DIV.children);
+            let selectedItems = [];
+
+            selectedItems = As.filter(A => A.dataset.menuSelected === 'true');
+
+            if (selectedItems.length > 0) {
+                let items = '';
+
+                selectedItems.forEach((o, i) => {
+                    if (i === selectedItems.length - 1) {
+                        items += o.innerText;
+                    } else {
+                        items += o.innerText + ', ';
+                    }
+                });
+
+                INPUT_TEXT.value = items;
+            } else {
+                INPUT_TEXT.value = '';
+            }
+            // BUG deslecting items via menu should empty close the menu
         }
-        // BUG deslecting items via menu should empty close the menu
-        console.log('v1')
-    }
 
-    // update menu items based on input text
-    INPUT_TEXT.addEventListener('keyup', (event) => updateMenu(event.target.value));
+        // update menu items based on input text
+        INPUT_TEXT.addEventListener('keyup', (event) => updateMenu(event.target.value));
 
-    function updateMenu(inputTxtValue) {
-        const As = Array.from(DIV.children);
+        function updateMenu(inputTxtValue) {
+            const As = Array.from(DIV.children);
 
-        // if text field is filled out display matches else display all items
-        if (inputTxtValue !== '') {
-            As.forEach(A => {
-                if ((A.innerText).toLowerCase().includes((inputTxtValue).toLowerCase())) {
+            // if text field is filled out display matches else display all items
+            if (inputTxtValue !== '') {
+                As.forEach(A => {
+                    if ((A.innerText).toLowerCase().includes((inputTxtValue).toLowerCase())) {
+                        A.style.display = 'block';
+                    } else {
+                        A.style.display = 'none';
+                    }
+                });
+            } else {
+                As.forEach(A => {
                     A.style.display = 'block';
-                } else {
-                    A.style.display = 'none';
-                }
-            });
-        } else {
-            As.forEach(A => {
-                A.style.display = 'block';
-            });
-        }
+                });
+            }
 
-        DIV.style.display = (inputTxtValue !== '') ? 'block' : 'none';
+            DIV.style.display = (inputTxtValue !== '') ? 'block' : 'none';
+        }
+    } else {
+        console.warn('Custom menu has no data!');
     }
+
 }
 
 // listen for clicks outside of menu, and broadcast custom event if true
