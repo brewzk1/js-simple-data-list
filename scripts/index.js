9948f00b81498ad8b4eecc2fb8ced775de3fcbb3
CH Folio <ghchfolio@gmail.com>
2023-09-08 01:09:38 -0500

    remove 2nd example

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,4 @@ import comboBox from './combobox.js';
 const fruitsArr = ['Apple', 'Apricot', 'Banana', 'Blueberry', 'Cantaloupe', 'Cherry', 'Coconut', 'Date', 'Grape', 'Guava', 'Jackfruit', 'Kiwi', 'Kumquat', 'Lemon', 'Mango', 'Nectarine', 'Orange', 'Papaya', 'Parsimmon', 'Pear', 'Peach', 'Pinapple', 'Pomegranate', 'Raspberry', 'Strawberry', 'Sugar Cane'];
 
 comboBox('fruits1Menu', fruitsArr); // single select
-comboBox('fruits2Menu', fruitsArr); // single select
-comboBox('fruits3Menu', fruitsArr, true); // multiple select
-
-// TODO multiple: optionally show check for selected items
-// TODO menu should be on topmost layer
+comboBox('fruits2Menu', fruitsArr, true); // multiple select
